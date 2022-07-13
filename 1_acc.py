5da5c50ea21061b6c9f85ea693297d5cb92b7915
romanoffnv <romanoffnv@gmail.com>
2022-07-13 23:53:17 +0300

    Excel is not altered anymore

--- 1_acc.py
+++ 1_acc.py
@@ -3,40 +3,64 @@ import os
 import re
 from pprint import pprint
 import pandas as pd
+from functools import reduce
 
 class ParseAcc:
     def __init__(self):
-        # self.xl = xl
-        # self.wb = wb
-        # self.ws1 = ws1
         # Get the Excel Application COM object
         self.xl = EnsureDispatch('Excel.Application')
         self.wb = self.xl.Workbooks.Open(f"{os.getcwd()}\\acc.xls")
-        # Sheets = wb.Sheets.Count
         self.ws1 = self.wb.Worksheets(1)
         
-        # inserting row and copying stuff or repsonsible persons, while clearing off original column
-        rangeObj = self.ws1.Range("C1:C5")
-        rangeObj.EntireColumn.Insert()
 
     def transCol(self):
-        
+        # getting mols, items with breaks (for mols) into lists
         row = 14
-        while self.ws1.Cells(row, 2).Value != None:
-            if self.ws1.Cells(row, 2).Font.Bold == False:
-                # print(ws1.Cells(row, 2))
-                self.ws1.Cells(row, 3).Value = self.ws1.Cells(row, 2).Value
-                self.ws1.Cells(row, 2).Value = None
-            row += 1
-        endrow = row
-        row = 14
-        while row != endrow:
+        L_mols_scratch, L_items = [], []
+        while True:
             if self.ws1.Cells(row, 2).Font.Bold == True and ("вич" in self.ws1.Cells(row, 2).Value or "вна" in self.ws1.Cells(row, 2).Value):
-                # re.findall("(вич)|(вна)", ws1.Cells(row, 2).Value):
                 print(self.ws1.Cells(row, 2).Value)
+                L_mols_scratch.append(self.ws1.Cells(row, 2).Value)
+                L_items.append('****')
+            elif self.ws1.Cells(row, 2).Font.Bold != True:
+                L_items.append(self.ws1.Cells(row, 2).Value)
+            if self.ws1.Cells(row, 2).Value == None:
+                break
             row += 1
+        
+        L_items = L_items[1:]
+        L_items.append('****')
+        
+        L_counts = []
+        counter = 0
+        
+        for i in L_items:
+            if i == '****':
+                L_items.remove(i)
+                L_counts.append(counter)
+                counter = 0
+            counter += 1    
+                # continue
+        # L_counts = [int(x) - 1 for x in L_counts if x == L_counts[-1:]]
+        
+        sumL_counts = reduce(lambda x, y: x + y, L_counts)
+        
+        # print(L_items[:15])
+        # print(len(L_items))
+        print(f'this is mols len {len(L_mols_scratch)}')
+        print(f'this is items len {len(L_items)}')
+        print(f'this is sum of items  {sumL_counts}')
+        print(f'this is the list of ranges {L_counts}')
+        # the last item of the L_counts list is 24, when should be 23 - problem needs to be addressed
+        print(f'this is the length of ranges list {len(L_counts)}')
+        
         self.wb.Close(True)
         self.xl.Quit()
+        
+        # after mols_scratch, items and counts lists are collected, L_mols should be populated by
+        # mulitplying each mols_scratch element by counts
+        L_mols = [((i + '**').split('**') * j).filter('', L_mols) for i, j in (zip(L_mols_scratch, L_counts))]
+        print(L_mols)
 
 parseAcc = ParseAcc()
 transfer = parseAcc.transCol()
